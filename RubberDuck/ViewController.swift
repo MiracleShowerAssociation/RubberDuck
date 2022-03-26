2edec6470ba1c0580221aa8114d5753ead654f0b
Song Jiyeon <jiyeon.song@riiid.co>
2022-03-26 11:20:54 +0900

    Initial Commit

---
+++ RubberDuck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RubberDuck
+//
+//  Created by Jiyeon Song on 2022/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
